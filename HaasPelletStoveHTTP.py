e06aa6acdf0c4f13882b3fffc0225bf611c457a9
Lukas Tomek <l.tomek@kostal.com>
2024-02-05 16:56:24 +0100

    Get prg state, prepare for sending.

--- HaasPelletStoveHTTP.py
+++ HaasPelletStoveHTTP.py
@@ -1,23 +1,93 @@
-from requests import get
+
+import hashlib
 import json
+from requests import get
 
 class HttpConection():
     """__init__() functions as the class constructor"""
     def __init__(self, IP):
         # Variables
         self.deviceStates = [];                 # Used to internally buffer the retrieved states before writing them to the adapter
-        self.noOfConnectionErrors = 0;       # Counter for connection problems
-        self.missingState = False;           # If a device state cannot be mapped to an internal state of the adapter, this variable gets set
-        self.timer = 0;                          # Settimeout-Pointer to the poll-function
-        self.disableAdapter = False;         # If an error occurs, this variable is set to true which disables the adapter
-        self.hw_version = 0;                     # Hardware version retrieved from the device
-        self.sw_version = 0;                     # Software version retrieved from the device
-        self.hpin = 0;                           # HPIN is the 'encrypted' PIN of the device
-        self.hspin = 0;                          # HSPIN is the secret, depending on the current NONCE and the HPIN
-        self.nonce = 0;                          # The current NONCE of the device
-        self.adapter = 0;                        # Adapter object
-        self.mode = '';
+        self.noOfConnectionErrors = 0           # Counter for connection problems
+        self.missingState = False               # If a device state cannot be mapped to an internal state of the adapter, this variable gets set
+        self.timer = 0                          # Settimeout-Pointer to the poll-function
+        self.disableAdapter = False             # If an error occurs, this variable is set to true which disables the adapter
+        self.hw_version = 0                     # Hardware version retrieved from the device
+        self.sw_version = 0                     # Software version retrieved from the device
+        self.hpin = 0                           # HPIN is the 'encrypted' PIN of the device
+        self.hspin = 0                          # HSPIN is the secret, depending on the current NONCE and the HPIN
+        self.nonce = 0                          # The current NONCE of the device
+        self.adapter = 0                        # Adapter object
+        self.prg = ''
+        self.mode = ''
+        self.ip = IP
         self.url = 'http://' + IP + '/status.cgi'
+        self.headers = {}
+        
+    # Handle a state change
+    def handleStateChange(self, id, state):
+        # Warning, state can be null if it was deleted
+        print('stateChange ' + id + ' ' + state)
+        # print('stateChange ' + id + ' ' + JSON.stringify(state));
+    
+        # you can use the ack flag to detect if it is status (true) or command (false)
+        # if (state and !state.ack):
+        #     adapter.log.debug('stateChange (command): ' + id + ' ' + JSON.stringify(state));
+        #
+        #     if (String(id) === (adapter.namespace + '.device.prg')) {
+        #         # Set new program
+        #         const post_data_prg = '{"prg":' + state.val + '}';
+        #
+        #         # Perform request
+        #         request.post({
+        #             headers: createHeader(post_data_prg),
+        #             url:     'http://' + adapter.config.fireplaceAddress + '/status.cgi',
+        #             body:    post_data_prg
+        #         }, function(error, response, body) {
+        #             adapter.log.debug('POST response: ' + response + ' [RESPONSE]; ' + body + ' [BODY]; ' + error + ' [ERROR];');
+        #
+        #             # POST was successful, perform ack
+        #             if (error === null && response.statusCode === 200) {
+        #                 # Acknowledge command
+        #                 adapter.setState(adapter.namespace + '.device.prg', state.val, true);
+        #             # POST was not successful, revert
+        #             } else {
+        #                 adapter.log.error('stateChange (command): ' + id + ' ' + JSON.stringify(state) + ' was not successful');
+        #                 adapter.log.error('POST response: ' + response + ' [RESPONSE]; ' + body + ' [BODY]; ' + error + ' [ERROR];');
+        #             }
+        #
+        #             # Poll new state to update nonce immediately
+        #             pollDeviceStatus();
+        #         });
+        #     } else if (String(id) === (adapter.namespace + '.device.sp_temp')) {
+        #         # Set new program
+        #         const post_data_sp_temp = '{"sp_temp":' + state.val + '}';
+        #
+        #         # Perform request
+        #         request.post({
+        #             headers: createHeader(post_data_sp_temp),
+        #             url:     'http://' + adapter.config.fireplaceAddress + '/status.cgi',
+        #             body:    post_data_sp_temp
+        #         }, function(error, response, body) {
+        #             adapter.log.debug('POST response: ' + response + ' [RESPONSE]; ' + body + ' [BODY]; ' + error + ' [ERROR];');
+        #
+        #             # POST was successful, perform ack
+        #             if (error === null && response.statusCode === 200) {
+        #                 // Acknowledge command
+        #                 adapter.setState(adapter.namespace + '.device.sp_temp', state.val, true);
+        #             # POST was not successful, revert
+        #             } else {
+        #                 adapter.log.error('stateChange (command): ' + id + ' ' + JSON.stringify(state) + ' was not successful');
+        #                 adapter.log.error('POST response: ' + response + ' [RESPONSE]; ' + body + ' [BODY]; ' + error + ' [ERROR];');
+        #             }
+        #
+        #             # Poll new state to update nonce immediately
+        #             pollDeviceStatus();
+        #         });
+        #     }
+        # }
+
+        
     def syncState(self, state, path):
         print('Syncing state of the oven')
         
@@ -34,7 +104,10 @@ class HttpConection():
 
             print('Nonce: {}'.format(state['meta']['nonce']))
             print('Mode: {}'.format(state['mode']))
+            self.prg = state['prg']
             self.mode = state['mode']
+            self.nonce = state['meta']['nonce']
+            
         except Exception as e:
             # Dump error and stop adapter
             print('Error syncing states: ' + e)
@@ -79,8 +152,39 @@ class HttpConection():
             #         pollDeviceStatus()
             #     }, adapter.config.pollingInterval * 1000)
             # }
+            
         # Synchronize the retrieved states with the states of the adapter
+    # Given the HPIN and the current NONCE, the HSPIN is calculated
+    # HSPIN = MD5(NONCE + HPIN)
+    def calculateHSPIN(self, NONCE, HPIN):
+        result = hashlib.md5(NONCE + HPIN);
+        print('HSPIN: {}'.format(HPIN));
+        return result;
     
+    # The PIN of the device is used to calculate the HPIN
+    # HPIN = MD5(PIN)
+    def calculateHPIN(self, PIN):
+        result = hashlib.md5(PIN);
+        print('HPIN: {}'.format(result));
+        return result;
+    
+    def createHeader(self, post_data):
+        self.headers = {
+            'Host':    self.ip,
+            'Accept':    '*/*',
+            'Proxy-Connection':    'keep-alive',
+            'X-BACKEND-IP':    'https://app.haassohn.com',
+            'Accept-Language': 'de-DE;q=1.0, en-DE;q=0.9',
+            'Accept-Encoding': 'gzip;q=1.0, compress;q=0.5',
+            'token': '32bytes',
+            'Content-Type': 'application/json',
+            'Content-Length': len(post_data),
+            'User-Agent': 'ios',
+            'Connection':    'keep-alive',
+            'X-HS-PIN': self.hspin
+            }
+         
+            
 def main():
     parser = HttpConection() 
     parser.pollDeviceStatus()
